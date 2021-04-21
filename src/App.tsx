2ff2c6d3ffa711d980f8704376d34fcb13844601
Shunnosuke Fujii <u270077f@alumni.osaka-u.ac.jp>
2021-04-21 23:58:56 +0900

    pra

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import LinePlot from './components/LinePlot';
 import PiePlot from './components/PiePlot';
+import BarPlot from './components/BarPlot';
+import RadarPlot from './components/RadarPlot';
+import BubblePlot from './components/BubblePlot';
+import MaterialUI from './components/MaterialUI';
 
 function App() {
   return (
     <div className="App">
-      <LinePlot/>
+      {/* <LinePlot/>
       <PiePlot/>
+      <BarPlot/>
+      <RadarPlot/>
+      <BubblePlot/> */}
+
+      <MaterialUI/>
     </div>
   );
 }
